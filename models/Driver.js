16885e14127a0e800a341111ca0c48fe8b16fd57
Sohan Reddy <gantalasohanreddy@gmail.com>
2024-07-24 14:25:33 -0500

    fix issue

--- models/Driver.js
+++ models/Driver.js
@@ -16,12 +16,10 @@ const DriverSchema = new mongoose.Schema({
   location: {
     type: {
       type: String,
-      enum: ['Point'],
-      required: true
+      enum: ['Point']
     },
     coordinates: {
       type: [Number],
-      required: true,
       index: '2dsphere'
     },
     formattedAddress: String
