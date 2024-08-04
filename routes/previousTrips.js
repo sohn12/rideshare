4f6fa54c5e483e4eb8d1b68b5c41995a4e32dca1
priya <gantlapriya4@gmail.com>
2024-08-04 09:02:38 -0500

    deleted commented code

--- routes/previousTrips.js
+++
@@ -1,9 +0,0 @@
-// const express = require("express");
-// const { getRidesByUser, getRidesByDriver } = require("../controllers/rides");
-
-// const router = express.Router();
-
-// router.get("/users/:id", getRidesByUser);
-// router.get("/drivers/:id", getRidesByDriver);
-
-// module.exports = router;
